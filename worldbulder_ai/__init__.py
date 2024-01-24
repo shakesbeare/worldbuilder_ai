338a00e15b7dceb4c343b6c4fdafaffe2afa8f24
Adria <adria.bower@yetanotheruseless.com>
2024-01-23 23:10:24 -0700

    added chat history template from docs, doesnt output anything.

--- worldbulder_ai/__init__.py
+++ worldbulder_ai/__init__.py
@@ -1,3 +1,2 @@
-from dotenv import dotenv_values
-
-config = dotenv_values(".env")
+from dotenv import load_dotenv
+load_dotenv()
--- worldbulder_ai/__main__.py
+++ worldbulder_ai/__main__.py
@@ -3,6 +3,7 @@ import openai
 from langchain import hub
 from langchain.agents import AgentExecutor, AgentType, load_tools, initialize_agent, create_react_agent
 from langchain_community.chat_models import ChatAnyscale
+from langchain.prompts.prompt import PromptTemplate
 from langchain.prompts import PromptTemplate
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -14,10 +15,14 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.schema import format_document
+from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
+from langchain_core.runnables import RunnableParallel
 
-from dotenv import dotenv_values
+#env variables, only for api key right now
+from dotenv import load_dotenv
+load_dotenv()
 
-config = dotenv_values(".env")
 
 vectorstore = FAISS.from_texts(
     ["harrison worked at kensho"], embedding=OpenAIEmbeddings()
@@ -41,4 +46,97 @@ chain = (
 )
 
 
-chain.invoke("where did harrison work?")
+#print(chain.invoke("where did harrison work?"))
+
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+
+Answer in the following language: {language}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = (
+    {
+        "context": itemgetter("question") | retriever,
+        "question": itemgetter("question"),
+        "language": itemgetter("language"),
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+#print(chain.invoke({"question": "what is kensho?", "language": "english"}))
+
+#### ^ all of this works ####
+#### v this is the stuff where the ai uses history to talk about stuff ####
+
+
+###### history stuff: #####
+
+
+_template = """
+
+Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:
+
+"""
+
+
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
+
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+
+
+def _combine_documents(
+    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
+):
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
+
+_inputs = RunnableParallel(
+    standalone_question=RunnablePassthrough.assign(
+        chat_history=lambda x: get_buffer_string(x["chat_history"])
+    )
+    | CONDENSE_QUESTION_PROMPT
+    | ChatOpenAI(temperature=0)
+    | StrOutputParser(),
+)
+_context = {
+    "context": itemgetter("standalone_question") | retriever | _combine_documents,
+    "question": lambda x: x["standalone_question"],
+}
+conversational_qa_chain = _inputs | _context | ANSWER_PROMPT | ChatOpenAI()
+
+conversational_qa_chain.invoke(
+    {
+        "question": "where did harrison work?",
+        "chat_history": [],
+    }
+)
+
+AIMessage(content='Harrison was employed at Kensho.')
+print(
+conversational_qa_chain.invoke(
+    {
+        "question": "where did he work?",
+        "chat_history": [
+            HumanMessage(content="Who wrote this notebook?"),
+            AIMessage(content="Harrison"),
+        ],
+    }
+)
+)
+#AIMessage(content='Harrison worked at Kensho.')
